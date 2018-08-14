9b1c224c8fcdca6bfa33da08a13b69f8e1d76497
A. Koch <dasantonym@users.noreply.github.com>
2018-08-14 18:58:53 +0200

    add delete to uploads

--- src/lib/uploads.js
+++ src/lib/uploads.js
@@ -32,6 +32,14 @@ class Uploads extends TinyEmitter {
         })
       })
     })
+
+    app.delete('/uploads/:file', async (req, res) => {
+      upload.single('file')(req, res, async () => {
+        const minioClient = new Minio.Client(config.assets.client)
+        await minioClient.removeObject(config.assets.bucket, filename, req.params.file)
+        _this._response(req, res)
+      })
+    })
   }
 
   _response (req, res, data = {}) {
